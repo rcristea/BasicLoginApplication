627f90f2a1fc91f9835f2292d7fd4d453bde4713
rcristea <rubendcristea@gmail.com>
2019-12-24 10:24:48 -0500

    Added signed in panel

---
+++ BasicLoginApplication/Custom Controls/SignedInPanel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicLoginApplication.Custom_Controls {
+    public partial class SignedInPanel : UserControl {
+        public SignedInPanel() {
+            InitializeComponent();
+        }
+
+        public void setWelcomeText(string username) {
+            labelWelcome.Text = "Welcome " + username;
+        }
+    }
+}
