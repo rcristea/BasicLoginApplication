3cb96b03ebed1cf81f04c3b7256a76e152cd0d76
rcristea <rubendcristea@gmail.com>
2019-12-24 10:25:42 -0500

    Fixed signed in panel background color

--- BasicLoginApplication/Custom Controls/SignedInPanel.Designer.cs
+++ BasicLoginApplication/Custom Controls/SignedInPanel.Designer.cs
@@ -44,6 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.Controls.Add(this.labelWelcome);
             this.Name = "SignedInPanel";
             this.Size = new System.Drawing.Size(364, 67);
