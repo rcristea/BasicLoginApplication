fbadf44200b8f2853e6fda612dd824c9da18af88
rcristea <rubendcristea@gmail.com>
2019-12-26 20:49:34 -0500

    Added comments to SignedInPanel
    Removed unused using statments.

--- BasicLoginApplication/Custom Controls/SignedInPanel.cs
+++ BasicLoginApplication/Custom Controls/SignedInPanel.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace BasicLoginApplication.Custom_Controls {
+    /// <summary>
+    ///     Used when the user has successfully signed in.
+    /// </summary>
     public partial class SignedInPanel : UserControl {
+        /// <summary>
+        ///     Creates a new SignedInPanel.
+        /// </summary>
         public SignedInPanel() {
             InitializeComponent();
         }
 
+        /// <summary>
+        ///     Used to set the text of the label to the user's username.
+        /// </summary>
+        /// <param name="username">The user's username.</param>
         public void setWelcomeText(string username) {
             labelWelcome.Text = "Welcome " + username;
         }
